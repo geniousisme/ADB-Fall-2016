faed395422f00e80f93979a244526532f46710f8
Chris Hsu <chia-hao.hsu@aiesec.net>
2016-10-28 22:55:34 -0400

    Init Content Summary part

---
+++ PA2/ContentSummarizer.py
@@ -0,0 +1,13 @@
+'''
+[TODO] Need to Deal With Duplicate url in root url set.
+'''
+
+class ContentSummarizer(object):
+	def __init__(self):
+		pass
+
+	def __fetch_page(self, url):
+		pass
+
+	def summarize(self):
+		pass
