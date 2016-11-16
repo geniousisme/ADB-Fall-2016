a7fb46df20a7412230ee5f381d5760a3a4dce096
Chris Hsu <chia-hao.hsu@aiesec.net>
2016-11-16 00:03:38 -0500

    Start associate rules extractor

--- PA3/AssocRulesExtractor.py
+++ PA3/AssocRulesExtractor.py
@@ -1,10 +1,15 @@
+from Util import get_candidates
+
 class AssocRulesExtractor(object):
     def __init__(self, filename, min_support, min_confidence):
-        self.dataset = filename
+        self.filename = filename
         self.min_supp = min_support
         self.min_conf = min_confidence
 
     def apriori(self):
-        pass
+        candidates = get_candidates(self.filename)
+        
+
 
     def run(self):
+        pass
