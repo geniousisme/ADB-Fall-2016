bf1877bd36ac59efdd57be93357c53665eaf37bf
Chris Hsu <chia-hao.hsu@aiesec.net>
2016-11-16 00:04:07 -0500

    Finish CSV reading function

--- PA3/Util.py
+++ PA3/Util.py
@@ -1,4 +1,21 @@
 import csv
+import re
 
-def get_dataset(filename):
-    pass
+def get_candidates(filename):
+    with open(filename, 'r') as csvfile:
+        csv_reader = csv.reader(csvfile, delimiter=',')
+        result = []
+        for row in csv_reader:
+            row_result = []
+            for item in row:
+                item = replace_non_ascii_with_space(item)
+                item = item.strip('\n').strip('\r')
+                row_result.append(item)
+            result.append(row_result)
+    return result
+
+def replace_non_ascii_with_space(input_doc):
+    return re.sub(r'[^\x00-\x7F]+', ' ', input_doc)
+
+if __name__ == "__main__":
+    print get_dataset('NYC_Wi-Fi_Hotspot_Locations_Map.csv')[:10]
