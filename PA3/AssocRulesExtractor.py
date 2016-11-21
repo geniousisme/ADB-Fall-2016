1485e93f356b7c5ce67e5f3824f74de89438b2d4
Chris Hsu <chia-hao.hsu@aiesec.net>
2016-11-21 00:21:57 -0500

    Finish L1 itemset generate

--- PA3/AssocRulesExtractor.py
+++ PA3/AssocRulesExtractor.py
@@ -1,30 +1,35 @@
-from collection import defaultdict
+from collections import defaultdict
 
 from Candidate import Candidate, ItemSet
 from Util import get_transactions
 
-
 class AssocRulesExtractor(object):
-    def __init__(self, filename, min_support, min_confidence):
+    def __init__(self, filename, min_supp, min_conf):
         self.filename = filename
-        self.min_supp = min_support
-        self.min_conf = min_confidence
-        self.total_trans_num = 0
-        self.support = defaultdict(list)
-        self.confidence = {}
+        self.min_supp = min_supp
+        self.min_conf = min_conf
+        self.total_trans = 0
 
-    def gen_L1_itemset(self):
+    def get_L1_itemset(self):
+        l1_itemset = Candidate()
         transactions = get_transactions(self.filename)
         self.total_trans = len(transactions)
+        
         for trans in transactions:
-            for candidate in trans:
-                
+            for item in trans:
+                l1_itemset[set([item])] = (
+                    l1_itemset.get(set([item])) + 1.0 / self.total_trans
+                )
+        candidateK = []
+        for itemset in l1_itemset:
+            if l1_itemset.get(itemset) >= self.min_supp:
+                candidateK.append(itemset)
 
     def apriori_gen(self):
         pass
     
     def apriori(self):
-        self.gen_L1_itemset()
+        self.get_L1_itemset()
 
     def run(self):
-        pass
+        self.apriori()
