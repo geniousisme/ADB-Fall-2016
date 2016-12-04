a0e4e6f59e0a9a544d40af5615552312502c20d4
Chris Hsu <chia-hao.hsu@aiesec.net>
2016-12-04 15:50:35 -0500

    Add data split function & verify function

--- PA3/data.py
+++ PA3/data.py
@@ -27,8 +27,39 @@ def data_normalize(filename):
             csv_writer.writerow(row)
             is_csv_field = False
     output.close()
+
+def data_split(filename):
+    output = file('splitted.csv', 'w')
+    is_csv_field = True
+    count_idx = 4
+    with open(filename, 'r') as csvfile:        
+        csv_reader = reader(csvfile, delimiter=',')
+        csv_writer = writer(output, dialect='excel')
+        for row in csv_reader:
+            if is_csv_field:
+                csv_writer.writerow(row)
+                is_csv_field = False
+            else:
+                count = int(row[count_idx])
+                while count:
+                    csv_writer.writerow(row[:count_idx] + row[count_idx + 1:])
+                    count -= 1
+    output.close()
+
+def test_splitted_data(filename):
+    with open(filename, 'r') as csvfile:        
+        csv_reader = reader(csvfile, delimiter=',')
+        total_row_num = 5
+        for row in csv_reader:
+            if len(row) != total_row_num:
+                print row
+                break
+    print "All rows are valid!"
+
                 
 if __name__ == "__main__":
-    data_normalize(sys.argv[1])
+    # data_normalize(sys.argv[1])
+    # data_split(sys.argv[1])
+    test_splitted_data(sys.argv[1])
     # you are a fucking asshole. why do you care their problem! stupid girl?!!! go to hell!    
     # The guy will be deadddddddddd.
